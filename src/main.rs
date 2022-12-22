a8fa8936080ae497c550de825463337eb9ebf2d9
Tr4shL0rd <ketil04@gmail.com>
2022-12-22 17:24:24 +0100

    reading files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::fs;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    //let mut contents: String 
+    let mut contents: String = String::from("");
+    for (_, arg) in args.iter().enumerate() {
+        //println!("args {}: {}", i , arg)
+        contents = fs::read_to_string(arg)
+            .expect("Error reading file");
+    }
+    println!("file contains: {}", contents);
+
 }
